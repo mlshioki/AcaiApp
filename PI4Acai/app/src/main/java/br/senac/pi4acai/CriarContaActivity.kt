52833ab1f1126d6413cdbed6308127e196d81151
guidezena <guilhermepereiradezena@gmail.com>
2021-09-29 21:03:00 -0300

    atualizando telas

---
+++ PI4Acai/app/src/main/java/br/senac/pi4acai/CriarContaActivity.kt
@@ -0,0 +1,11 @@
+package br.senac.pi4acai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CriarContaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criar_conta)
+    }
+}
---
+++ PI4Acai/app/src/main/java/br/senac/pi4acai/EsqueceuSenhaActivity.kt
@@ -0,0 +1,11 @@
+package br.senac.pi4acai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EsqueceuSenhaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_esqueceu_senha)
+    }
+}
